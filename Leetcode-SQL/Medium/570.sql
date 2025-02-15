f85b7cf312a27c203c796a533f385c3a374b7cab
Abhinav Krishnamoorthy <abhinav14kr@zohomail.com>
2025-02-14 23:47:41 -0800

    daily updates

--- Leetcode-SQL/Medium/570.sql
+++ Leetcode-SQL/Medium/570.sql
@@ -1,3 +1,29 @@
+570. Managers with at Least 5 Direct Reports
+
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| department  | varchar |
+| managerId   | int     |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the name of an employee, their department, and the id of their manager.
+If managerId is null, then the employee does not have a manager.
+No employee will be the manager of themself.
+ 
+
+Write a solution to find managers with at least five direct reports.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+SOLUTION:
+
 # Write your MySQL query statement below
 
 WITH first_cte as 
