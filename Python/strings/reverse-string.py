48b41af44a98efd66c071db36e90d89a0af012c7
Abhinav Krishnamoorthy <abhinav14kr@zohomail.com>
2025-03-15 20:36:56 -0700

    daily commit

---
+++ Python/strings/reverse-string.py
@@ -0,0 +1,14 @@
+# Method: Using slicing
+
+
+s = 'Tom'
+output = s[::-1]
+print(output)
+
+
+# Method: Using reversed() function
+
+s = input("Enter any name: ")
+r = reversed(s)
+output  = ''.join(r)
+print(output)
