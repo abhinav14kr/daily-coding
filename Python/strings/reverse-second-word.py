0e83fb9084f70e8f9f4d2720e6a1345aecdbaa58
Abhinav Krishnamoorthy <abhinav14kr@zohomail.com>
2025-03-18 23:38:41 -0700

    daily commit

--- Python/strings/reverse-second-word.py
+++ Python/strings/reverse-second-word.py
@@ -1,17 +1,14 @@
- s = 'one two three four five six'
- s1 = s.split()
- print(s1)
+s = 'one two three four five six'
+l = s.split()
+i = 0
+l1 = []
 
- i = 0
- l1 = []
-
- while i < len(s1): 
-     if i%2 == 0: 
-
+while i < len(l): 
+    if i % 2 == 0: 
         l1.append(l[i])
     else: 
         l1.append(l[i][::-1])
-    i = i+1
+    i = i + 1
 
-output = ' '.join(la)
+output = ' '.join(l1)
 print(output)
