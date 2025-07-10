84a28afbb5461ef51eee8335ec35af41214c33d8
Abhinav K <abhinav14kr@zohomail.com>
2025-07-10 15:03:32 -0700

    daily commit

--- pandas/easy/data-aggregation-1741.py
+++ pandas/easy/data-aggregation-1741.py
@@ -19,13 +19,13 @@ import pandas as pd
 
 def total_time(employees: pd.DataFrame) -> pd.DataFrame:
 
-    # Renaming 'event_day' to 'day' (ensure no extra spaces in column name)
+    # Renaming 'event_day' to 'day' coz that's the expected output format from leetcode
     employees = employees.rename(columns={'event_day': 'day'})
     
     # Calculating  total time spent in the office
     employees['total_time'] = employees['out_time'] - employees['in_time']
     
-    # Group by 'day' and 'emp_id', sum the total time spent, then sorting by 'emp_id'
+    # Grouping by 'day' and 'emp_id', sum the total time spent, sorting by 'emp_id'
     result = employees.groupby(['day', 'emp_id'])['total_time'].sum().reset_index().sort_values(by='emp_id', ascending=True)
     
     return result
