241f57d88dc4208ba5c2aa14222116fe5a0d4a45
Abhinav K <abhinav14kr@zohomail.com>
2025-07-15 23:55:31 -0700

    restructuring some code

--- SQL/leetcode/Medium/570.sql
+++ SQL/leetcode/Medium/570.sql
@@ -20,6 +20,7 @@ No employee will be the manager of themself.
 Write a solution to find managers with at least five direct reports.
 Return the result table in any order.
 The result format is in the following example.
+
 '''
 
 
