20ce613f4c144091140b6d7b1e7b04add2fcfa9e
Abhinav K <abhinav14kr@zohomail.com>
2025-07-24 00:35:51 -0700

    .

--- Python/data-structures/dictionary/dict-conditional.py
+++ Python/data-structures/dictionary/dict-conditional.py
@@ -1,4 +1,34 @@
+'''
+You're given a list of student scores:
+scores = [
+    ("Alice", 85),
+    ("Bob", 92),
+    ("Charlie", 78),
+    ("David", 64),
+    ("Eve", 99),
+    ("Frank", 55),
+    ("Grace", 70),
+    ("Hannah", 88),
+]
 
+Group students into categories based on their score:
+
+Score Range	Category
+90 and above	"Excellent"
+75 to 89	"Good"
+60 to 74	"Average"
+Below 60	"Poor"
+
+Your final output should be a dictionary like this:
+
+{
+    "Excellent": ["Bob", "Eve"],
+    "Good": ["Alice", "Charlie", "Hannah"],
+    "Average": ["Grace", "David"],
+    "Poor": ["Frank"]
+}
+
+'''
 scores = [
         ("Alice", 85),
         ("Bob", 92),
