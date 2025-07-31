80e04bd550f5150cc6a513c5ecd721a292f16f1b
Abhinav K <abhinav14kr@zohomail.com>
2025-07-31 00:48:34 -0700

    little restructuring

---
+++ python/dictionary/dict-count-products-by-customer.py
@@ -0,0 +1,13 @@
+purchases = [
+    ("Alice", "Laptop"),
+    ("Bob", "Mouse"),
+    ("Alice", "Keyboard"),
+    ("Charlie", "Shampoo"),
+    ("Bob", "Monitor"),
+    ("Alice", "Notebook"),
+    ("Charlie", "Toothpaste"),
+    ("Alice", "Pen"),
+    ("Bob", "Tablet"),
+]
+
+
